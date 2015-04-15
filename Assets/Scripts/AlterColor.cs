ceae30310a0808dfcd22baee7d21c6da5e16ce98
GSamuel <gideon_hoog@hotmail.com>
2015-04-15 18:14:52 +0200

    added box

---
+++ Assets/Scripts/AlterColor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlterColor : MonoBehaviour {
+
+	public Color color;
+
+	// Use this for initialization
+	void Start () {
+		Renderer rend = GetComponent<Renderer>();
+		rend.material.SetColor("_EmisColor", color);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/boxColor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boxColor : MonoBehaviour
+{
+
+	public Color color;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		foreach (Renderer rend in GetComponentsInChildren<Renderer> ())
+		{
+			rend.material.SetColor ("_Color", color);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
